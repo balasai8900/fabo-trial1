32abcc160ce1c4be6e836743f79d2358cb927961
balasai8900 <118534815+balasai8900@users.noreply.github.com>
2024-01-11 12:39:40 +0530

    Update SampleProjectApplicationTests.java

--- src/test/java/com/practice/sampleproject/SampleProjectApplicationTests.java
+++ src/test/java/com/practice/sampleproject/SampleProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SampleProjectApplicationTests {
+class SampleProjectApplicationTests extends springservletcontainerinitializer{
 
 	@Test
 	void contextLoads() {
