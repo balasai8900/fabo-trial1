1f72350de7ecf27f9740f3973d69ce542174f5dd
balasai8900 <118534815+balasai8900@users.noreply.github.com>
2024-01-22 15:53:10 +0530

    Update SampleProjectApplication.java

--- src/main/java/com/pack/fabo/SampleProjectApplication.java
+++ src/main/java/com/pack/fabo/SampleProjectApplication.java
@@ -2,12 +2,16 @@ package com.pack.fabo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class SampleProjectApplication {
+public class SampleProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectApplication.class, args); 
 	}
-
+@override
+protected springApplicationBuilder configure(SpringApplicationBuilder application) {
+ return application.sources(SecondInitialize.class);
+}
 }
